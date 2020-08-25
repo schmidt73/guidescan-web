eb76871fe02d87278767ed3acc7fbf6f7feb8ccd
Henri Schmidt <packerfan73@gmail.com>
2020-08-25 12:21:36 -0400

    Implement parser dispatching and monadic error handling.

--- src/guidescan_web/routes.clj
+++ src/guidescan_web/routes.clj
@@ -8,40 +8,81 @@
    [compojure.core :refer :all]
    [guidescan-web.bam.db :as db]))
 
-(defn- parse-row
-  [[chr start-str end-str & _]]
-  [chr (Integer/parseInt start-str) (Integer/parseInt end-str)])
- 
-(defmulti parse-query
-  "Parses the query for easy look up in the guideRNA database. The
-   multimethod dispatches on the type of query, so it should handle raw
-   text input, BED files, GFF files, and FASTA files while being
-   extensible in the future. Raises a ParsingException on failure to
-   parse input.
+(defn failure?
+  [parse-tree]
+  (contains? parse-tree :failure))
 
-   The method returns output with the following form:
-   [[chrX1 start-1 end-1] [chrX2 start-2 end-2] ...]"
-  (fn [req] :text))
- 
-(defmethod parse-query :text
+(defn success?
+  [parse-tree]
+  (contains? parse-tree :success))
+
+(defn- parse-line
+  "Parses one line of a text file, returning a parse object indicating
+   success or failure along with an error message."
+  [line-number line]
+  (if-let [[_ chr start-str end-str] (re-find #"(chr.*):(\d+)-(\d+)" line)]
+    {:success
+      [chr (Integer/parseInt start-str) (Integer/parseInt end-str)]}
+    {:failure (str "Failed to parse: \"" line "\" on line " (+ 1 line-number))}))
+
+(defn parse-raw-text
+  "Parses the raw text of a query, either coming
+   from a text file or from a query box."
   [text]
-  (->> text
-       (clojure.string/split-lines)
-       (map #(rest (re-find #"(chr.*):(\d+)-(\d+)" %)))
-       (map parse-row)))
+  (let* [parsed-lines
+         (->> (clojure.string/split-lines text)
+              (map-indexed parse-line)) 
+         failed-lines (filter failure? parsed-lines)]
+    (if (not-empty failed-lines)
+      (first failed-lines)
+      {:success (map :success parsed-lines)})))
+
+(defn get-query-type
+  "Returns the type of query."
+  [req-params]
+  (if-let [filename (get-in req-params [:query-file-upload :filename])]
+    (if (not= filename "")
+      (cond
+        (re-find #"(?i).*\.txt" filename)           :text-file
+        (re-find #"(?i).*\.fasta" filename)         :fasta-file
+        (re-find #"(?i).*\.((gtf)|(gff))" filename) :gtf-file
+        (re-find #"(?i).*\.bed" filename)           :bed-file
+        :otherwise                                  :unknown-file)
+      :text)
+    :text))
+
+(defmulti parse-query
+  "Parses the parameters of a query for easy look up in the guideRNA
+   database. The multimethod dispatches on the type of query, and it
+   handles raw text input, BED files, GTF files, and FASTA files.
+
+   The method returns a map indicating either a successful parse tree
+   or failure to parse along with an error message.
+
+   A succesful parse tree looks like this:
+   [[chrX1 start-1 end-1] [chrX2 start-2 end-2] ...]"
+  get-query-type)
+
+(defmethod parse-query :text
+  [req-params]
+  (parse-raw-text (:query-text req-params)))
+
+(defmethod parse-query :text-file
+  [req-params]
+  (parse-raw-text (slurp (get-in req-params [:query-file-upload :tempfile]))))
 
 (defn query-route
   "Core of the Guidescan website. Exposes a REST api that takes a
    query in a variety of forms, parses it, and returns the response
    as a nested JSON object."
   [config req]
-  (try
-    (catch Parse))
-  (let [query (parse-query (:query (:params req)))
+  (let [result (parse-query (:params req))
         organism (:organism (:params req))]
-    (->> (map #(apply (partial db/query-bam config organism) %) query)
-         (map #(clojure.string/join "\n" %)) 
-         (clojure.string/join "\n\n\n"))))
+    (if-let [query (:success result)]
+      (->> (map #(apply (partial db/query-bam config organism) %) query)
+           (map #(clojure.string/join "\n" %)) 
+           (clojure.string/join "\n\n\n"))
+      (:failure result))))
 
 (defn create-routes
   [config]
