4d8a4503b89cf7965c870f0c2f7e9bab8ecf73c1
Henri Schmidt <packerfan73@gmail.com>
2021-07-21 17:03:47 -0400

    Update add sgRNA library script to reflect current Guidescan2 library design.

--- scripts/add_grna_lib.clj
+++ scripts/add_grna_lib.clj
@@ -14,20 +14,18 @@
         "             libraries (grna VARCHAR(1023) NOT NULL,"
         "                        organism VARCHAR(1023) NOT NULL,"
         "                        source VARCHAR(1023) NOT NULL,"
-        "                        gene_symbol VARCHAR(1023) NOT NULL,"
-        "                        identifier VARCHAR(1023) NOT NULL,"
-        "                        ots_summary VARCHAR(1023),"
-        "                        native_score REAL,"
-        "                        cfd_score REAL,"
-        "                        ce_score REAL,"
-        "                        PRIMARY KEY (gene_symbol, identifier));"]
-       (clojure.string/join "\n")))
-
-(def create-essential-genes-table
-  (->> ["CREATE TABLE IF NOT EXISTS"
-        "       essential_genes (gene_symbol VARCHAR(1023) NOT NULL,"
-        "                        organism VARCHAR(1023) NOT NULL,"
-        "                        PRIMARY KEY (gene_symbol, organism));"]
+        "                        gene_symbol VARCHAR(1023),"
+        "                        grna_type VARCHAR(1023) NOT NULL,"
+        "                        identifier VARCHAR(1023),"
+        "                        region_id VARCHAR(1023),"
+        "                        offtarget0 INT NOT NULL,"
+        "                        offtarget1 INT NOT NULL,"
+        "                        offtarget2 INT NOT NULL,"
+        "                        offtarget3 INT NOT NULL,"
+        "                        specificity REAL,"
+        "                        specificity_5pG REAL,"
+        "                        cutting_efficiency REAL,"
+        "                        PRIMARY KEY (gene_symbol, grna, grna_type));"]
        (clojure.string/join "\n")))
 
 (defmethod fmt/format-clause :on-conflict-nothing [[op v] sqlmap]
@@ -36,19 +34,24 @@
 (h/defhelper on-conflict-nothing [m args]
   (assoc m :on-conflict-nothing nil))
 
+(defn nil-parse-float [s] (if-not (empty? s) (Float/parseFloat s)))
+
 (defn create-sql-record-guides
   [organism csv-entry]
-  (let [native-score (get csv-entry "score")]
-    {:grna (get csv-entry "sgRNA")
-     :identifier (get csv-entry "identifier")
-     :native_score (if (= native-score "NA")
-                     nil
-                     (Float/parseFloat native-score))
-     :organism organism
-     :source (get csv-entry "source")
-     :cfd_score (Float/parseFloat (get csv-entry "cfd_score"))
-     :gene_symbol (get csv-entry "gene")
-     :ots_summary (get csv-entry "ots")}))
+  {:grna (get csv-entry "sgRNA")
+   :identifier (get csv-entry "Identifier")
+   :organism organism
+   :source "Guidescan2"
+   :region_id (get csv-entry "Cutting Region ID")
+   :specificity (nil-parse-float (get csv-entry "Specificity"))
+   :specificity_5pG (nil-parse-float (get csv-entry "5pG Specificity"))
+   :cutting_efficiency (nil-parse-float (get csv-entry "Cutting Efficiency"))
+   :gene_symbol (get csv-entry "Gene")
+   :grna_type (get csv-entry "Type")
+   :offtarget0 (Integer/parseInt (get csv-entry "0 Off-Targets"))
+   :offtarget1 (Integer/parseInt (get csv-entry "1 Off-Targets"))
+   :offtarget2 (Integer/parseInt (get csv-entry "2 Off-Targets"))
+   :offtarget3 (Integer/parseInt (get csv-entry "3 Off-Targets"))})
 
 (defn create-sql-statement-guides
   [organism csv-entries]
@@ -58,34 +61,22 @@
         (on-conflict-nothing)
         sql/format)))
 
-(defn create-sql-statement-genes
-  [organism essential-genes]
-  (-> (h/insert-into :essential_genes)
-      (h/values (map (fn [gene-sym] {:gene_symbol gene-sym :organism organism})
-                     essential-genes))
-      (on-conflict-nothing)
-      sql/format))
-
 (defn usage []
   (->> ["Adds a new guide-rna library to the database."
         ""
-        "Usage: java -jar add-grna-lib.jar [jdbc-url-string] [library-csv] [essential-genes] [organism]"]
+        "Usage: java -jar add-grna-lib.jar [jdbc-url-string] [library-csv] [organism]"]
        (clojure.string/join \newline)))
 
-(defn- main
+(defn -main
   [& args]
   (when (< (count args) 2)
     (println (usage))
     (System/exit 1))
   (let [ds (jdbc/get-datasource {:jdbcUrl (nth args 0)})
         library-csv (read-csv-with-header (nth args 1))
-        essential-genes-list (clojure.string/split-lines (slurp (nth args 2)))
-        organism (nth args 3)
+        organism (nth args 2)
         entries (partition 500 library-csv)]
     (with-open [conn (jdbc/get-connection ds)]
       (jdbc/execute! conn [create-grna-library-table])
-      (jdbc/execute! conn [create-essential-genes-table])
       (doseq [entry entries]
-        (jdbc/execute! conn (create-sql-statement-guides organism entry)))
-      (doseq [essential-genes (partition 500 essential-genes-list)]
-        (jdbc/execute! conn (create-sql-statement-genes organism essential-genes))))))
+        (jdbc/execute! conn (create-sql-statement-guides organism entry))))))
